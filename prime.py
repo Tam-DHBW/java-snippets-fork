c7976cb9305eb96141cbe59725e6c3202defdc5e
Atai Mamytov <atay.mamytov@gmail.com>
2025-01-29 14:15:35 +0100

    test 8

--- prime.py
+++ prime.py
@@ -3,9 +3,9 @@ def is_prime(num):
     if num <= 1:
         return False
     for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
+        if num % i == 0:asdfasdfasd
+            return Falseasdfasdfas
     return True
 
 print(is_prime(17))  # True
-print(is_prime(18))  # False
+print(is_prime(18))  # Falsefasdfasdfdfasdf
