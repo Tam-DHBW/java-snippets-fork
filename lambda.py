873f7cfdd2fd4bce3d49d7e8fec116c1d1eb755c
Atai Mamytov <atay.mamytov@gmail.com>
2025-01-29 20:22:00 +0100

    test 9

---
+++ lambda.py
@@ -0,0 +1,8 @@
+# Lambda function to add two numbers
+add = lambda x, y: x + y
+print(add(5, 3))  # Output: 8
+
+# Using lambda with sorted
+points = [(1, 2), (3, 1), (5, -1), (2, 4)]
+sorted_points = sorted(points, key=lambda point: point[1])
+print(sorted_points)  # Output: [(5, -1), (3, 1), (1, 2), (2, 4)]
