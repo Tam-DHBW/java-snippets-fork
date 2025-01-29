e5c36f64f37807a20414fa25f0b2c30dc185632e
Atai Mamytov <atay.mamytov@gmail.com>
2025-01-29 19:59:21 +0100

    test 8

---
+++ basic_class.py
@@ -0,0 +1,13 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def bark(self):
+        print(f"{self.name} says woof!")
+
+# Create an instance of Dog
+my_dog = Dog("Buddy", 3)
+print(my_dog.name)  # Output: Buddy
+print(my_dog.age)   # Output: 3
+my_dog.bark()       # Output: Buddy says woof!
