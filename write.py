5bbb0ba77dbaf5124c9c503dd15f58c229c35a60
Atai Mamytov <atay.mamytov@gmail.com>
2025-01-29 20:49:44 +0100

    test 9

--- write.py
+++ write.py
@@ -1,9 +1,3 @@
 # Writing to a file
 with open("example.txt", "w") as f:
     f.write("Hello from Python!\n")
-
-# Reading from a file
-with open("example.txt", "r") as f:
-    content = f.read()
-
-prin
