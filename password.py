e9bbdb0c3f87be01ecd25fda263e73fde523926b
Atai Mamytov <atay.mamytov@gmail.com>
2025-02-05 10:07:59 +0100

    Clear repository

--- password.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-def generate_password(length=8):
-    characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(length))
-    return password
-
-print(generate_password(12))  # Example Output: 'aB3$dE5&fG1!'
